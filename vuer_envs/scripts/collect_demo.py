a62cfec76899a7948dc6ec85b7519caeb73841b6
Ge Yang <ge.ike.yang@gmail.com>
2025-01-14 23:46:14 -0500

    add trajectory visualization

--- vuer_envs/scripts/collect_demo.py
+++ vuer_envs/scripts/collect_demo.py
@@ -1,4 +1,6 @@
+import colorsys
 from asyncio import sleep
+from collections import deque
 from datetime import datetime
 from glob import glob
 from os.path import join
@@ -7,9 +9,15 @@ from typing import List
 from params_proto import ParamsProto, Flag
 from termcolor import colored
 from vuer.events import ClientEvent
-from vuer.schemas import Box, span, Html, group
+from vuer.schemas import Box, span, Html, group, HandActuator, Line
 
 from vuer_envs.scripts.util.working_directory_context_manager import WorkDir
+
+
+def pi2_hsv(pi):
+    """return '#RRGGBB' for a given angle in radians."""
+    r, g, b = colorsys.hsv_to_rgb((pi % (2 * 3.14159)) / (2 * 3.14159), 1, 1)
+    return [int(r * 255), int(g * 255), int(b * 255)]
 
 
 class Params(ParamsProto, cli_parse=False):
@@ -75,6 +83,15 @@ def main():
 
     vuer = Vuer(static_root=args.wd, port=args.vuer_port)
 
+    is_loaded = False
+
+    @vuer.add_handler("ON_CONTRIB_LOAD")
+    async def on_contrib_load(event: ClientEvent, proxy: VuerSession):
+        nonlocal is_loaded
+
+        is_loaded = True
+        print("ON_CONTRIB_LOAD event", event.value)
+
     box_state = "#23aaff"
     box_other = "#ff2323"
 
@@ -98,9 +115,17 @@ def main():
             silent=True,
         )
 
+    mpos_history = deque(maxlen=200)
+    color_ticker = 0
+
     @vuer.add_handler("ON_MUJOCO_FRAME")
     async def on_mujoco_frame(event: ClientEvent, proxy: VuerSession):
+        nonlocal mpos_history
+
         frame = event.value["keyFrame"]
+
+        x, y, z = frame["mpos"]
+        mpos_history.append([x, z, -y])
 
         logger.log(
             ts=event.ts,
@@ -113,9 +138,36 @@ def main():
 
         @vuer.spawn(start=True)
         async def main(proxy: VuerSession):
+            nonlocal mpos_history, color_ticker
+
+            await sleep(10)
             # todo: add a ContribLoader to load the MuJoCo plugin.
-            proxy.upsert @ MuJoCo(key="default-sim", src=args.src, assets=args.asset_paths)
+            proxy.upsert @ MuJoCo(
+                HandActuator(key="pinch-on-squeeze"),
+                key="default-sim",
+                src=args.src,
+                assets=args.asset_paths,
+                # turn of light to make it run faster.
+                # useLights=False,
+                xpos=[],
+            )
+
             while True:
+                if mpos_history:
+                    print("showing the line")
+                    proxy.upsert @ Line(
+                        points=[*mpos_history],
+                        # note: change to 0.001 to view in VR, 1.0 to view on desktop.
+                        linewidth=1.0,
+                        vertexColors=[pi2_hsv((i + color_ticker) / 100) for i in range(len(mpos_history))],
+                        # color="#ff0000",
+                    )
+                    await sleep(0.100)
+                    last = mpos_history[-1]
+                    mpos_history.clear()
+                    mpos_history.append(last)
+                    color_ticker += mpos_history.__len__()
+
                 proxy.upsert @ group(
                     Html(
                         span("Click Me!!"),
@@ -131,16 +183,18 @@ def main():
                     position=[0, 0.5, 0],
                 )
 
-                await sleep(0.016)
+                await sleep(0.16)
 
     logger.job_completed()
 
 
 if __name__ == "__main__":
     # Params.wd = "/Users/yajvanravan/Library/CloudStorage/GoogleDrive-yravan@mit.edu/.shortcut-targets-by-id/1UQnuWv4ICaE50w_nPTTNeZ1_YtRsswuX/lucidxr-assets/development/robots"
-    Params.wd = "/Users/ge/Library/CloudStorage/GoogleDrive-ge.ike.yang@gmail.com/My Drive/lucidxr-assets/development/robots"
+    # Params.wd = "/Users/ge/Library/CloudStorage/GoogleDrive-ge.ike.yang@gmail.com/My Drive/lucidxr-assets/development/robots"
+    Params.wd = "/Users/ge/Downloads"
     Params.scene_name = "scene"
     Params.scene_folder = "universal_robots_ur5e"
+    Params.asset_prefix = "https://ge-2.ngrok.app/static"
 
     args = Params()
 
